3bf5ef748d6f97d28e94cdfaabc03ffb1c4b08d8
Hyperes <moostral3@yandex.ru>
2023-12-14 16:32:56 +0300

    Update db.py

--- db.py
+++ db.py
@@ -13,7 +13,7 @@ except:
     print('Can`t establish connection to database')
     
 def add_product(name: str, photo_path: str, price: float, old_price: float, url_product: str, sex: str, brand: str):
-    conn = psycopg2.connect('postgres://isrzeyjx:P4wzfAfwaiJmP_zCdYuvVPp0fcMkBvXl@cornelius.db.elephantsql.com/isrzeyjx')
+    conn = psycopg2.connect('postgres:')
     cursor = conn.cursor()
     sql = "INSERT INTO products (name, photo_path, price, old_price, url_product, date, sex, brand) VALUES ('%s', '%s', %s, %s, '%s', '%s', '%s', '%s') ON CONFLICT (url_product) DO NOTHING" % (name, photo_path, price, old_price, url_product, formatted_date, sex, brand)
     cursor.execute(sql)
