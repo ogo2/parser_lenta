29cd9670abfe7f9ecfe17c2d1e6bb77d9843314b
Hyperes <moostral3@yandex.ru>
2024-01-18 00:56:06 +0300

    Update db.py

--- db.py
+++ db.py
@@ -5,7 +5,7 @@ formatted_date = now.strftime('%Y-%m-%d %H:%M:%S')
 print(formatted_date)
 try:
     # пытаемся подключиться к базе данных
-    conn = psycopg2.connect('postgres:')
+    conn = psycopg2.connect('postgres:/')
     
     
 except:
@@ -15,7 +15,7 @@ except:
 def add_product(name: str, photo_path: str, price: float, old_price: float, url_product: str, sex: str, brand: str):
     conn = psycopg2.connect('postgres:')
     cursor = conn.cursor()
-    sql = "INSERT INTO products (name, photo_path, price, old_price, url_product, date, sex, brand) VALUES ('%s', '%s', %s, %s, '%s', '%s', '%s', '%s') ON CONFLICT (url_product) DO NOTHING" % (name, photo_path, price, old_price, url_product, formatted_date, sex, brand)
+    sql = """INSERT INTO products (name, photo_path, price, old_price, url_product, date, sex, brand) VALUES ('%s', '%s', %s, %s, '%s', '%s', '%s', '%s') ON CONFLICT (url_product) DO NOTHING""" % (name, photo_path, price, old_price, url_product, formatted_date, sex, brand)
     cursor.execute(sql)
     conn.commit()
     cursor.close()
