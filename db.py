f5185138c806bc3bb344a5269c89fd9c41a32de9
Hyperes <moostral3@yandex.ru>
2024-04-13 09:39:38 +0300

    Update db.py

--- db.py
+++ db.py
@@ -6,7 +6,7 @@ print(formatted_date)
 
     
 def add_product(name: str, photo_path: str, price: float, old_price: float, url_product: str, sex: str, brand: str):
-    conn = psycopg2.connect('postgres://isrzeyjx:P4wzfAfwaiJmP_zCdYuvVPp0fcMkBvXl@cornelius.db.elephantsql.com/isrzeyjx')
+    conn = psycopg2.connect('')
     cursor = conn.cursor()
     sql = """INSERT INTO products (name, photo_path, price, old_price, url_product, date, sex, brand) VALUES ('%s', '%s', %s, %s, '%s', '%s', '%s', '%s') ON CONFLICT (url_product) DO NOTHING""" % (name, photo_path, price, old_price, url_product, formatted_date, sex, brand)
     cursor.execute(sql)
